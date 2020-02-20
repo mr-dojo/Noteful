2d722afc063a801062f6c6402993dce089392cc6
Carlo Costantino <costantinoart@gmail.com>
2020-02-20 08:56:13 +0000

    Fixed getnotes for folder helper

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -20,6 +20,7 @@ export default class NoteListMain extends React.Component {
     const { folderId } = this.props.match.params;
     const { notes = [] } = this.context;
     const folderNotes = getNotesForFolder(notes, folderId);
+    console.log(folderNotes);
 
     return (
       <ErrorCheck>
--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -5,7 +5,9 @@ export const findNote = (notes = [], noteId) =>
   notes.find(note => parseInt(note.id) === parseInt(noteId));
 
 export const getNotesForFolder = (notes = [], folderId) =>
-  !folderId ? notes : notes.filter(note => note.folderId === folderId);
+  !folderId
+    ? notes
+    : notes.filter(note => parseInt(note.folder_id) === parseInt(folderId));
 
 export const countNotesForFolder = (notes = [], folderId) =>
   notes.filter(note => note.folder_id === folderId).length;
