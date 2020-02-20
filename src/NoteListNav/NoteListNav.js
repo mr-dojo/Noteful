3c725f8e194cd869b9b67c9749f61ee246b24eda
Carlo Costantino <costantinoart@gmail.com>
2020-02-20 04:14:37 +0000

    Fixed countNotesForFolder helper to use folder_id

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
-import CircleButton from '../CircleButton/CircleButton'
-import { countNotesForFolder } from '../notes-helpers'
-import StoreContext from '../storeContext'
-import ErrorCheck from '../ErrorCheck'
-import './NoteListNav.css'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import CircleButton from "../CircleButton/CircleButton";
+import { countNotesForFolder } from "../notes-helpers";
+import StoreContext from "../storeContext";
+import ErrorCheck from "../ErrorCheck";
+import "./NoteListNav.css";
 
 export default class NoteListNav extends React.Component {
-  static contextType = StoreContext
+  static contextType = StoreContext;
 
   render() {
-    const { folders=[], notes=[] } = this.context
+    const { folders = [], notes = [] } = this.context;
     return (
       <ErrorCheck>
-        <div className='NoteListNav'>
-          <div className='NoteListNav__button-wrapper'>
+        <div className="NoteListNav">
+          <div className="NoteListNav__button-wrapper">
             <CircleButton
               tag={Link}
-              to='/add-folder'
-              type='button'
-              className='NoteListNav__add-folder-button'
+              to="/add-folder"
+              type="button"
+              className="NoteListNav__add-folder-button"
             >
               <br />
               Add Folder
             </CircleButton>
           </div>
-          <ul className='NoteListNav__list'>
-            {folders.map(folder =>
+          <ul className="NoteListNav__list">
+            {folders.map(folder => (
               <li key={folder.id}>
                 <NavLink
-                  className='NoteListNav__folder-link'
+                  className="NoteListNav__folder-link"
                   to={`/folder/${folder.id}`}
                 >
-                  <span className='NoteListNav__num-notes'>
+                  <span className="NoteListNav__num-notes">
                     {countNotesForFolder(notes, folder.id)}
                   </span>
                   {folder.name}
                 </NavLink>
               </li>
-            )}
+            ))}
           </ul>
         </div>
       </ErrorCheck>
-    )
+    );
   }
 }
