64812754a77a975150b74a527a6e674f5a380e3c
Carlo Costantino <costantinoart@gmail.com>
2020-02-20 09:09:02 +0000

    Fixed AddFolder and AddNote

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,57 +1,60 @@
-import React from 'react'
-import StoreContext from '../storeContext'
-
-function randomId() {
-  return Math.random().toString()
-}
+import React from "react";
+import StoreContext from "../storeContext";
 
 export default class AddFolder extends React.Component {
-
-  static contextType = StoreContext
+  static contextType = StoreContext;
 
   state = {
     folders: this.context.folders
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    const value = e.target.folder.value
+    const value = e.target.folder.value;
     const newFolder = {
-      id: randomId(),
-      name: value,
-    }
-    fetch('http://localhost:9090/folders', {
-      method: 'POST',
+      name: value
+    };
+    fetch("http://localhost:8000/api/folders", {
+      method: "POST",
       body: JSON.stringify(newFolder),
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json"
       }
     })
-    .then(response => {
-      if(!response.ok) {
-        return new Error(response.message);
-      }
-      return response.json();
-    })
-    .then(() => {
-      this.context.addFolder(newFolder);
-      this.props.history.push('/');
-    })
-    .catch(err => {
-      throw new Error(err);
-    })
-  }
+      .then(response => {
+        if (!response.ok) {
+          return new Error(response.message);
+        }
+        return response.json();
+      })
+      .then(() => {
+        this.context.addFolder(newFolder);
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
 
   render() {
     return (
       <div className="form-container">
         <form className="folder-form" onSubmit={e => this.handleSubmit(e)}>
           <h1>New Folder</h1>
-          <label htmlFor="folder" className="form-label">Enter Folder Name</label>
-          <input type="text" name="folder" placeholder="Folder name here" required></input>
-          <button type="submit" className="form-submit">Save</button>
+          <label htmlFor="folder" className="form-label">
+            Enter Folder Name
+          </label>
+          <input
+            type="text"
+            name="folder"
+            placeholder="Folder name here"
+            required
+          ></input>
+          <button type="submit" className="form-submit">
+            Save
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,119 +1,105 @@
-import React from 'react'
-import StoreContext from '../storeContext'
-import "./AddNote.css"
-
-function randomId() {
-  return Math.random().toString()
-}
+import React from "react";
+import StoreContext from "../storeContext";
+import "./AddNote.css";
 
 export default class AddNote extends React.Component {
-
-  static contextType = StoreContext
+  static contextType = StoreContext;
 
   state = {
     notes: this.context.notes
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-    const name = e.target.name.value
-    const content = e.target.content.value
-    const folderId = e.target.folder.value
+    const name = e.target.name.value;
+    const content = e.target.content.value;
+    const folderId = e.target.folder.value;
     const newDate = new Date();
 
     const newNote = {
-      id: "note" + randomId(),
       name: name,
       modified: newDate.toISOString(),
-      folderId: folderId,
-      content: content,
-    }
+      folder_id: folderId,
+      content: content
+    };
 
-    fetch('http://localhost:9090/notes', {
-      method: 'POST',
+    fetch("http://localhost:8000/api/notes", {
+      method: "POST",
       body: JSON.stringify(newNote),
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json"
       }
     })
-    .then(response => {
-      if(!response.ok) {
-        return new Error(response.message);
-      }
-      return response.json();
-    })
-    .then(() => {
-      this.context.addNote(newNote);
-      this.props.history.push('/');
-    })
-    .catch(err => {
-      throw new Error(err);
-    })
-  }
+      .then(response => {
+        if (!response.ok) {
+          return new Error(response.message);
+        }
+        return response.json();
+      })
+      .then(() => {
+        this.context.addNote(newNote);
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
 
   render() {
     return (
       <div className="newNote-container">
         <form className="newNote-form" onSubmit={e => this.handleSubmit(e)}>
           <h1>New Note</h1>
-          <label 
-            htmlFor="name" 
-            className="newNote-label"
-          >
+          <label htmlFor="name" className="newNote-label">
             Name
           </label>
           <br />
-          <input 
-            type="text" 
-            name="name" 
-            placeholder="name here" 
+          <input
+            type="text"
+            name="name"
+            placeholder="name here"
             className="newNote-name"
-            aria-required="true" 
+            aria-required="true"
             required
           />
           <br />
-          <label 
-            htmlFor="content" 
-            className="newNote-label"
-          >
+          <label htmlFor="content" className="newNote-label">
             Content
           </label>
           <br />
-          <textarea 
-            name="content" 
-            className="newNote-content" 
-            rows="5" 
-            cols="10" 
-            placeholder="content here" 
+          <textarea
+            name="content"
+            className="newNote-content"
+            rows="5"
+            cols="10"
+            placeholder="content here"
             required
           />
           <br />
-          <label 
-            htmlFor="folder" 
-            className="newNote-label"
-          >
+          <label htmlFor="folder" className="newNote-label">
             Folder
           </label>
           <br />
-          <select 
+          <select
             name="folder"
-            className="newNote-folder" 
-            aria-required="true" 
+            className="newNote-folder"
+            aria-required="true"
             required
           >
             {this.context.folders.map(folder => {
-              return <option key={folder.id} value={folder.id}>{folder.name}</option>
+              return (
+                <option key={folder.id} value={folder.id}>
+                  {folder.name}
+                </option>
+              );
             })}
           </select>
           <br />
-          <button 
-            type="submit" 
-            className="form-submit"
-          >
+          <button type="submit" className="form-submit">
             Save
           </button>
         </form>
       </div>
-    )
+    );
   }
 }
